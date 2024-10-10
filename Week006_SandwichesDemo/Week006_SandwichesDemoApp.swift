362212cef01a05ee29f8e2ee1c5469edc5f94a28
LivioDR <liviodavidreinoso@yahoo.com.ar>
2024-10-10 16:02:01 -0400

    Initial Commit

---
+++ Week006_SandwichesDemo/Week006_SandwichesDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Week006_SandwichesDemoApp.swift
+//  Week006_SandwichesDemo
+//
+//  Created by Livio David Reinoso on 2024-10-10.
+//
+
+import SwiftUI
+
+@main
+struct Week006_SandwichesDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
